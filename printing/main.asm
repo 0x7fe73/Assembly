4b216b902b2ffe0a72ced42efde92dea4db5fb56
0x7fe73 <101475055+0x7fe73@users.noreply.github.com>
2022-03-12 11:04:41 -0800

    Create main.asm

---
+++ printing/main.asm
@@ -0,0 +1,23 @@
+; main.asm
+; Author: 0x7fe73
+; Date: 12/03/2022
+
+global _start
+
+section .text
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, mlength
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+    ret
+
+
+section .data
+    message db "Hello World!", 12
+    mlength equ $ - message
